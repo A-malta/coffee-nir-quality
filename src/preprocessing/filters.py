b6e5d609a07c7b49945518d2eed0a4e4fab464e8
Aline <malta.a.aline@gmail.com>
2025-11-30 22:48:13 -0300

    feat: add moving average and sg filters and update mean centering

--- src/preprocessing/filters.py
+++ src/preprocessing/filters.py
@@ -12,12 +12,17 @@ def check_scipy():
         raise ImportError("A biblioteca 'scipy' é necessária para esta função. Instale-a com 'pip install scipy'.")
 
 def snv(X: pd.DataFrame) -> pd.DataFrame:
+
     mu = X.mean(axis=0)
     sigma = X.std(axis=0, ddof=1).replace(0.0, np.nan)
     return ((X - mu) / sigma).fillna(0.0)
 
-def mean_centering(X: pd.DataFrame) -> pd.DataFrame:
-    mu = X.mean(axis=1)
+def mean_centering(X: pd.DataFrame, mean_vector: Optional[pd.Series] = None) -> pd.DataFrame:
+
+    if mean_vector is None:
+        mu = X.mean(axis=1)
+    else:
+        mu = mean_vector
     return X.sub(mu, axis=0)
 
 def savitzky_golay(X: pd.DataFrame, window_length: int, polyorder: int) -> pd.DataFrame:
@@ -49,3 +54,10 @@ def savitzky_derivative(
         col: savgol_filter(X[col].to_numpy(), window_length, polyorder, deriv=deriv, delta=delta)
         for col in X.columns
     }, index=X.index)
+
+def moving_average(X: pd.DataFrame, window_length: int) -> pd.DataFrame:
+    return X.rolling(window=window_length, axis=1, center=True).mean().fillna(method='bfill', axis=1).fillna(method='ffill', axis=1)
+
+def sg_smoothing(X: pd.DataFrame, window_length: int, polyorder: int = 0) -> pd.DataFrame:
+
+    return savitzky_golay(X, window_length, polyorder)
